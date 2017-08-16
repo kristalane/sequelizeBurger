c3cf36260fc70e6c86a6ba8d6a6c51a91d655f6c
Krista Lane <klane@Kristas-MacBook-Air.local>
2017-08-15 23:30:50 -0700

    remove force:true from sync

--- server.js
+++ server.js
@@ -24,10 +24,7 @@ app.set("view engine", "handlebars");
 require("./routes/api-routes.js")(app);
 require("./routes/html-routes.js")(app);
 
-// var routes = require("./controllers/burger_controller.js")
-// app.use("/", routes);
-
-db.sequelize.sync({force:true}).then(function() {
+db.sequelize.sync().then(function() {
   app.listen(PORT, function(){
     console.log("App listening on PORT " + PORT);
   });
